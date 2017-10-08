4417ee63bdd7a0699bafab545bd5ab575f826279
bertderbecker <bertbecker1970@yahoo.com>
2017-10-08 13:32:39 +0200

    fix bug

--- src/main/scala/io/github/bertderbecker/scalapfui/property/NestedProperty.scala
+++ src/main/scala/io/github/bertderbecker/scalapfui/property/NestedProperty.scala
@@ -2,21 +2,21 @@ package io.github.bertderbecker.scalapfui.property
 
 import scala.language.higherKinds
 
-trait NestedProperty[T, CC[X] <: Property[X]] extends Property[T] {
+trait NestedProperty[T] extends Property[T] {
 
-  val nestedWritableProperty: NestedWritableProperty[T, CC]
+  val writableProperty: WritableProperty[T]
 
-  val nestedReadableProperty: NestedReadableProperty[T, CC]
+  val readableProperty: ReadableProperty[T]
 
 
-  override def doBinding(other: ReadableProperty[T]): Unit = nestedWritableProperty.bindTo(other)
+  override def doBinding(other: ReadableProperty[T]): Unit = writableProperty.bindTo(other)
 
-  override def calcValue: Option[T] = nestedReadableProperty.value
+  override def calcValue: Option[T] = readableProperty.value
 
-  override def processOnChange(op: (ReadableProperty[T], T, T) => Unit): Unit = nestedReadableProperty.onChangeFull(op)
+  override def processOnChange(op: (ReadableProperty[T], T, T) => Unit): Unit = readableProperty.onChangeFull(op)
 
-  override def removeOnChange(op: (ReadableProperty[T], T, T) => Unit): Unit = nestedReadableProperty.removeOnChange(op)
+  override def removeOnChange(op: (ReadableProperty[T], T, T) => Unit): Unit = readableProperty.removeOnChange(op)
 
-  override def doUpdate(newValue: T): Unit = nestedWritableProperty.doUpdate(newValue)
+  override def doUpdate(newValue: T): Unit = writableProperty.doUpdate(newValue)
 
 }
